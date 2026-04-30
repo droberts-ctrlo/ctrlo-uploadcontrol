7b0a5128995c0854f372473e9e6c46fdd2d7c237
Dave Roberts <d.roberts@ctrlo.com>
2026-04-30 13:00:06 +0100

    Update to module and fix tests

--- jest.config.cjs
+++ jest.config.cjs
@@ -176,13 +176,15 @@ const config = {
   // testRunner: "jest-circus/runner",
 
   // A map from regular expressions to paths to transformers
-  // transform: undefined,
+  transform: {
+    "^.+\\.[jt]sx?$": "babel-jest"
+  },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  // transformIgnorePatterns: [
-  //   "/node_modules/",
+  transformIgnorePatterns: [
+    "!/node_modules/@ctrlo/",
   //   "\\.pnp\\.[^\\/]+$"
-  // ],
+  ],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
